1956b8f3c2a7d492d67f7df8cac975bccf95e0d6
Sebastian Soler <the212co@gmail.com>
2015-05-31 12:21:04 -0400

    finished v1 of content and keyword match data

---
+++ app/contentMatch.js
@@ -0,0 +1,24 @@
+var contentMatch = {
+
+  feature: [
+    "",
+    "",
+  ],
+
+  funding: [
+    "",
+    "",
+  ],
+
+  market: [
+    "",
+    "",
+  ],
+
+  growth: [
+    "hire",
+    "hiring",
+  ]
+
+
+}
--- app/models/tweet.js
+++ app/models/tweet.js
@@ -2,16 +2,19 @@ var mongoose = require('mongoose');
 
 var tweetSchema = mongoose.Schema({
 
-	created_at		: String,
-	tweet_id		: Number,
-	text			: String,
-	t_user_id		: Number,
-	t_user_name		: String,
-	urls 			: { type : Array , "default" : [] },
-	hashtags		: { type : Array , "default" : [] },
-	user_mentions	: { type : Array , "default" : [] },
-	date_added		: Date
-	
+	created_at			: String,
+	tweet_id			: Number,
+	text				: String,
+	t_user_id			: Number,
+	t_user_name			: String,
+	urls 				: { type : Array , "default" : [] },
+	hashtags			: { type : Array , "default" : [] },
+	user_mentions		: { type : Array , "default" : [] },
+	portfolio_company	: String,
+	competitor			: String,
+	key_words			: { type : Array , "default" : [] },
+	date_added			: Date
+
 });
 
 module.exports = mongoose.model('Tweet', tweetSchema);
